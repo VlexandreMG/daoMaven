bf1d674308c32def755124b558c92a3255e63948
VlexandreMG <dodolashmou@gmail.com>
2026-04-14 05:22:45 +0300

    Midebug package

---
+++ src/main/java/com/mrNaina/Main.java
@@ -0,0 +1,14 @@
+import com.mrNaina.models.Olona;
+import com.mrNaina.dao.GenericDao;
+import com.mrNaina.dao.GenericDaoImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        GenericDao<Olona> olonadao = new GenericDaoImpl<>();
+
+        Olona olona = new Olona();
+        olona.setNom("test");
+        olonadao.save(olona);
+    }
+}
